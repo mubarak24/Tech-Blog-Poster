e9e4c3ab9e2e2120fdf26737579e66cc8d1175e5
mubarak <mubarakcali24@gmail.com>
2023-05-28 13:54:01 -0700

    signing up

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -95,7 +95,7 @@ router.get('/new', (req, res) => {
 router.get('/signup', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/signup');
+    res.redirect('/login');
     return;
   }
 
