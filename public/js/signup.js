b0aa42c7fed5aeee73ffd421fd2f876797a128bc
mubarak <mubarakcali24@gmail.com>
2023-05-28 14:20:31 -0700

    sign-up fixed

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,13 +1,14 @@
 const signupFormHandler = async (event) => {
     event.preventDefault();
 
-    const name = document.querySelector('input[name="email"]').value.trim();
+    const name = document.querySelector('input[name="username"]').value.trim();
+    const email = document.querySelector('input[name="email"]').value.trim();
     const password = document.querySelector('input[name="password"]').value.trim();
 
-    if (name && password) {
+    if (name && email && password) {
         const response = await fetch('/api/users', {
             method: 'POST',
-            body: JSON.stringify({ name, password }),
+            body: JSON.stringify({ name, email, password }),
             headers: { 'Content-Type': 'application/json' },
         });
 
@@ -16,6 +17,8 @@ const signupFormHandler = async (event) => {
         } else {
             alert('Sign up failed. Please try again.');
         }
+    } else {
+        alert('Please provide a name, email, and password.');
     }
 };
 
