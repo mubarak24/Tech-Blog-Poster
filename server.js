c60905012fd057e1219303af6f5222d3ba497390
mubarak <mubarakcali24@gmail.com>
2023-05-29 13:24:03 -0700

    heroku fix

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
+const sequelize = require('./config/config');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
